169c295fd74220112eb1fafff5a0717c19e84838
zllxxl <zllxxl@gmail.com>
2018-11-08 15:51:01 +0800

    commit database base model and sqlexcuse

--- www/app.py
+++ www/app.py
@@ -1,20 +1,17 @@
-import logging; logging.basicConfig(level=logging.INFO)
-
-import asyncio, os, json, time
-from datetime import datetime
-
+import logging
+import asyncio
 from aiohttp import web
+logging.basicConfig(level=logging.INFO)
 
 
 def index(request):
-    return web.Response(body='<h1>Awesome</h1>')
+    return web.Response(body=b'<h1>Awesome</h1>')
 
 
-@asyncio.coroutine
-def init(loop):
+async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('Get', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000')
     return srv
 
